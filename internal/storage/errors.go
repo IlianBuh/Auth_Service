23ae6d7fd8e33e9a6a907a10482a28cf454d9f69
IlianBuh <ilianbuhovec@gmail.com>
2025-05-10 18:51:30 +0000

    feat: add following errors

--- internal/storage/errors.go
+++ internal/storage/errors.go
@@ -6,4 +6,6 @@ var (
 	ErrNotFound       = errors.New("user is not found")
 	ErrUserExists     = errors.New("user already exists")
 	ErrInvalidUserKey = errors.New("unknown type of user key")
+	ErrFollowing      = errors.New("user is already following")
+	ErrNoFollowing    = errors.New("user has not followed")
 )
